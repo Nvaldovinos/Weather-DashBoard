3a036ad1419d27e93abe0444f06fe7876f493cf5
Naomi Valdovinos <n.valdovinos74@gmail.com>
2021-05-08 21:46:33 -0400

    edeting

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 //variables for elments int he DOM
 var searchInput = document.getElementById('search-input');
 var searchButton = document.getElementById('search-button');
-var formEl = document.getElementById('search-form');
-var citiesEl = document.getElementById('searched-cities');
+
 
 //variables for api
 var apiKey = 'd71c1be9151a89df5059df5d7300ceb8';
 var apiURLRoot = 'https://api.openweathermap.org/data/2.5/weather?';
+var dailyForecast ='https://api.openweathermap.org/data/2.5/onecall?lat=35.7721&lon=-78.6386&&units=imperial&exclude=current,minutely,hourly&appid=12ab451d86c37bd1bbaa8df17ff823aa';
 
 
 //functions 
-function getWeather (){
+function getWeather(){
     console.log('getWeather is working');
     //get name of city 
     var cityName = searchInput.value;
@@ -23,26 +23,25 @@ function getWeather (){
         })
         .then(function(data){
             console.log(data);
-        })
-       
-        //form submissions 
-        function formSubmit(event){
-                event.preventDefault();
-                var citySearch = $('input[name="search-input"]').val();
-                citiesEl.append('<li>' + citySearch + '<li>')
-                $('input[name="search-input"]').val('')
-            }
-            
-       
+        })   
 }
-//Prints list of cities 
-// function formSubmit(event){
-//     event.preventDefault();
-//     var citySearch = $('input[name="search-input"]').val();
-//     citiesEl.append('<li>' + citySearch + '<li>')
-//     $('input[name="search-input"]').val('')
-// }
 
+function getForecast(){
+    console.log('get forecast is working');
+    
+}
+// function getApi(requestUrl) {
+//     fetch(requestUrl)
+//       .then(function (response) {
+//         console.log(response);
+//         if (response.status === 200) {
+//           responseText.textContent = response.status;
+//         }
+//         return response.json();
+//     });
+//   }
+  
+//   getApi(requestUrl);
 
 //event listeners 
 searchButton.addEventListener('click', getWeather);
